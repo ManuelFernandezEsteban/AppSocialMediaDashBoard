ba394a863c53d6b4a594f5ad6abf3c5494cbe890
Manuel1977 <Manuel_fe1977@hotmail.com>
2022-06-01 20:18:16 +0100

    resolucion bug

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ Users should be able to:
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Solution URL: [Repo Git hub](https://github.com/ManuelFernandezEsteban/appSocialMediaDashboard.git)
 - Live Site URL: [Add live site URL here](https://your-live-site-url.com)
 
 ## My process
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { environment } from 'src/environments/environment';
+//import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 import { Observable } from 'rxjs';
 
 const baseUrl=environment.baseUrl;
